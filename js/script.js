4b776e4f53097a0c628f6d6e6be15a47ce7e10db
RUzai <RUzairov@gmail.com>
2022-02-02 17:21:15 +0300

    Modal updates, minor changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-$(document).ready(function() {
-    $('.header__burger').click(function(event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-    $('.header__list').click(function(event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
+$(document).ready(function () {
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+  $('.header__list').click(function (event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
 });
+
+$(document).ready(function () {
+  $('.order').click(function () {openModal()});
+  //$('.modal').click(function () {openModal()});
+})
+
+function openModal() {
+  $('.modal').toggleClass('active');
+  $('body').toggleClass('lock');
+}
